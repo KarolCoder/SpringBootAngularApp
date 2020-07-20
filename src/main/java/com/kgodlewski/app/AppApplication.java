6d3f13122e69027e683853b3012b663da233d32d
KarolG <godlewski215@gmail.com>
2020-07-20 20:27:01 +0200

    Changed structure from JAR to WAR

--- src/main/java/com/kgodlewski/app/AppApplication.java
+++ src/main/java/com/kgodlewski/app/AppApplication.java
@@ -3,11 +3,14 @@ package com.kgodlewski.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.ServletRequest;
+
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+
 	}
 
 }
