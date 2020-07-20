3426c3a36ddb0e50015386907637496ef3ef9aba
KarolG <godlewski215@gmail.com>
2020-07-20 21:30:03 +0200

    Added displayLocation view

--- src/main/java/com/kgodlewski/app/controllers/LocationController.java
+++ src/main/java/com/kgodlewski/app/controllers/LocationController.java
@@ -1,14 +1,40 @@
 package com.kgodlewski.app.controllers;
 
+import com.kgodlewski.app.entities.Location;
+import com.kgodlewski.app.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class LocationController {
 
+    @Autowired
+    LocationService service;
+
     @RequestMapping("/showCreate")
     public String showCreate(){
         return "createLocation";
+    }
+
+    @RequestMapping("/saveLoc")
+    public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap){
+        Location locationSaved = service.saveLocation(location);
+        String msg = "Location saved with id: " + locationSaved.getId();
+        modelMap.addAttribute("msg",msg);
+        return "createLocation";
+    }
+
+    @RequestMapping("/displayLocations")
+    public String displayLocations(ModelMap modelMap){
+        List<Location> allLocations = service.getAllLocations();
+        System.out.println(allLocations);
+        modelMap.addAttribute("allLocations",allLocations);
+        return "displayLocations";
     }
 
 
