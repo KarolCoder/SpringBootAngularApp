fec5756fddbcfe72126a92158e09b505d00fac4d
KarolG <godlewski215@gmail.com>
2020-08-03 19:54:23 +0200

    Added Email Sender and Generate Chart utils

--- src/main/java/com/kgodlewski/app/entities/Location.java
+++ src/main/java/com/kgodlewski/app/entities/Location.java
@@ -9,7 +9,6 @@ import javax.persistence.Id;
 public class Location {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String code;
     private String name;
---
+++ src/main/java/com/kgodlewski/app/util/ReportUtil.java
@@ -0,0 +1,8 @@
+package com.kgodlewski.app.util;
+
+import java.util.List;
+
+public interface ReportUtil {
+
+    void generatePieChart(String path, List<Object[]> data);
+}
